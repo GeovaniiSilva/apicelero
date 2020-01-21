bb0292d76ae5ff67d6566e9d3695109d8818ff2a
GeovaniiSilva <geapsi@gmail.com>
2020-01-21 15:37:27 -0300

    Models redefinition

--- ogames/models.py
+++ ogames/models.py
@@ -41,7 +41,7 @@ class Sport(models.Model):
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='events')
 
     def __str__(self):
